e352a353ec62ea52001c0ee73b5536c772025abe
bebebrrr <kubanova04@gmail.com>
2023-09-27 15:07:25 +0300

    Program

---
+++ prog.cpp
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<<"Hello!";
+cout<<"This program is designed to add numbers";
+cout<<"Enter some numbers";
+int a, b, c, d;
+cout<<"a =";
+cin>>a;
+cout<<"b =";
+cin>>b;
+cout<<"c =";
+cin>>c;
+d=a+b+c; 
+}
+=======
+>>>>>>> b8902ac90ab1dba5fa593249b53c346789c98dc3
